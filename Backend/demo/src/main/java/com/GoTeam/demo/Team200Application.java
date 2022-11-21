8de4dec6c71aab3e11a5855602d07757732d251a
cheyenneabel <113124769+cheyenneabel@users.noreply.github.com>
2022-11-21 13:39:43 -0500

    basic project structure

---
+++ Backend/demo/src/main/java/com/GoTeam/demo/Team200Application.java
@@ -0,0 +1,13 @@
+package com.GoTeam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team200Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team200Application.class, args);
+	}
+
+}
---
+++ Backend/demo/src/test/java/com/GoTeam/demo/Team200ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GoTeam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team200ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
