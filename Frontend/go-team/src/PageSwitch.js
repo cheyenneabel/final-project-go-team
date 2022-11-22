6cd444dd503a28ddb444f7763e46314ed51cae4d
cheyenneabel <113124769+cheyenneabel@users.noreply.github.com>
2022-11-22 14:00:39 -0500

    basic layout

--- Frontend/go-team/src/PageSwitch.js
+++ Frontend/go-team/src/PageSwitch.js
@@ -1,7 +1,7 @@
 import { Route, Switch} from 'react-router-dom';
 import MainPage from './components/MainPage';
 import TennisSchedulePage from './components/TennisSchedulePage';
-import Team200ContactPage from './components/Team200ContactPage';
+import AboutUsPage from './components/AboutUsPage';
 
 //import all components (minus header and footer)
 
@@ -11,7 +11,7 @@ const PageSwitch = () => {
         <Switch>
             <Route exact path = "/" component={MainPage} />
             <Route exact path = "/tennisSchedule" component={TennisSchedulePage} />
-            <Route exact path = "/team200" component={Team200ContactPage} />
+            <Route exact path = "/aboutUs" component={AboutUsPage} />
         </Switch>
     )
 }
---
+++ Frontend/go-team/src/components/TennisSchedulePage.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const TennisSchedulePage = () => (
+    <div className="tennisSchedulePage">
+        <h2 id="scheduleGreeting">Tennis Match Signup Page</h2>
+    </div>
+)
+
+export default TennisSchedulePage;
