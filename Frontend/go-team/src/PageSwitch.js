4e304fa95f32f1d6949dee9b797979cf1786f478
don90omar <113124856+don90omar@users.noreply.github.com>
2022-11-23 09:58:40 -0500

    update user page

--- Frontend/go-team/src/PageSwitch.js
+++ Frontend/go-team/src/PageSwitch.js
@@ -2,6 +2,8 @@ import { Route, Switch} from 'react-router-dom';
 import MainPage from './components/MainPage';
 import TennisSchedulePage from './components/TennisSchedulePage';
 import Team200ContactPage from './components/Team200ContactPage';
+import UserPage from './components/UserPage';
+
 
 //import all components (minus header and footer)
 
@@ -12,6 +14,7 @@ const PageSwitch = () => {
             <Route exact path = "/" component={MainPage} />
             <Route exact path = "/tennisSchedule" component={TennisSchedulePage} />
             <Route exact path = "/team200" component={Team200ContactPage} />
+            <Route excat path={'/user'} component={UserPage}/>
         </Switch>
     )
 }
