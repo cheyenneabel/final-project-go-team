3836bbb0c143740d82c0a1e692ac264db2f8ef3f
cheyenneabel <113124769+cheyenneabel@users.noreply.github.com>
2022-11-26 13:21:03 -0500

    added comments to js pages

--- Frontend/go-team/src/components/UserPage.js
+++ Frontend/go-team/src/components/UserPage.js
@@ -2,8 +2,14 @@ import React from "react";
 
 
 const UserPage = () => {
+
+    {
+        // potentially we will use this page to display a user's account info and allow them to edit their info
+    }
     return (
         <h1>this is a UserPage</h1>
+
+    
     )
 }
 
