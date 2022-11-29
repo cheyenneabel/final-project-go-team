764b40b55f991231f98cc26038ae6a6ea1e5f34f
cheyenneabel <113124769+cheyenneabel@users.noreply.github.com>
2022-11-29 10:08:09 -0500

    small changes

--- Frontend/go-team/src/components/UserPage.js
+++ Frontend/go-team/src/components/UserPage.js
@@ -5,6 +5,7 @@ const UserPage = () => {
 
     {
         // potentially we will use this page to display a user's account info and allow them to edit their info
+        // skill level, favorite sport, position, age, best time to play
     }
     return (
         <h1>this is a UserPage</h1>
