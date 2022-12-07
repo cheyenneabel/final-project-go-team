af22c9f9b9ab9c60f078f76c840ae96cbc6e5dfb
cheyenneabel <113124769+cheyenneabel@users.noreply.github.com>
2022-12-07 11:45:24 -0500

    login signup styling

--- Frontend/go-team/src/components/DescriptionPage.js
+++ Frontend/go-team/src/components/DescriptionPage.js
@@ -8,13 +8,13 @@ const DescriptionPage = () => {
             <h3>Bring It Home</h3>
                 <p className="moreInfo">Welcome to Go Team! We are passionate about forming a local recreational sports community.
                 Have you ever wanted to play a game, but you're down one person? Or what if you want
-                to make some new friends and start a weekly game at a local park?
-
+                to make some new friends and start a weekly game at a local park?<br></br>
+                <br></br>
                 Go Team is an application to help people in the area play more games. Once you have
                 an account, you will select the sport you are interested in playing. Then you are given
                 the option to join an available game or schedule a new game. Users can search for
-                a game by skill level or location.
-
+                a game by skill level or location.<br></br>
+                <br></br>
                 We hope that this is an accessible and fun way for our community to play more sports!
                     
                 </p>
